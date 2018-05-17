97825568f6ddf2e8afc8848418a514e92a521d7c
Anwesh43 <anweshthecool0@gmail.com>
2018-05-17 11:23:24 +0530

    adding LinkedRotatingLineStage class

---
+++ LinkedRotatingLineStage.ts
@@ -0,0 +1,30 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+
+class LinkedRotatingLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    draw(context) {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
